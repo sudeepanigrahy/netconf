79e7b87d96ea3d2c334bf4dad71481eac7109ac1
SudeePanigrahy <sudeep.panigrahy10@gmail.com>
2023-12-13 17:56:13 +0530

    Update switch_info.py

--- switch_info.py
+++ switch_info.py
@@ -1,6 +1,6 @@
-kwargs = {"host": "10.70.126.152",
+kwargs = {"host": "********",
           "port": "830",
-          "username": "spanigrahy",
-          "password": "Sueme@0128"
+          "username": "********",
+          "password": "*******"
           }
 
